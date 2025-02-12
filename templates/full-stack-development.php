ba9e2475aab479d5bd2c10dc7fd11c55263af237
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-02-12 15:12:28 +0600

    fullstack development page completed

--- templates/full-stack-development.php
+++ templates/full-stack-development.php
@@ -1,30 +1,42 @@
 <!-- header section starts -->
 <div style="margin-top: 60px; margin-bottom: 50px;">
+    <div class="position-relative">
+        <div class="dots-block-img position-absolute">
+            <img class="dots-img" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/dot-image.png"
+                alt="">
+        </div>
+    </div>
+
     <div class="container">
         <div class="row flex-lg-row flex-column-reverse p-2">
             <div class="col-md-12 col-lg-6 pt-md-3">
                 <div>
-                    <div class="sub-title text-center text-lg-start">Full-Stack Development</div>
-                    <div class="title text-center text-lg-start">Unleashing potential with Full-Stack Development
-                        solutions</div>
-                    <p class="paragraph text-center text-lg-start mt-4">At Innova Soft Ltd., we believe in the power of
-                        Full-Stack Development to propel businesses forward. Our tailored solutions empower you to
-                        innovate, adapt, and thrive in a dynamic digital landscape. From concept to execution, trust us
-                        to transform your vision into reality.</p>
-                    <div class="text-center text-lg-start">
-                        <a href="<?php echo esc_url(home_url('index.php/contact-us')); ?>"
-                            class="btn btn-success card-btn mt-4">Let's
-                            talk <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                stroke-width="3" stroke="currentColor" style="width: 18px; height: 16px;">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
-                            </svg></a>
+                    <div class="title text-center text-lg-start innova-color-230">Full-Stack Development</div>
+                    <div class="sub-section">
+                        <div class="sub-section-title text-left text-uppercase text-lg-start innova-color-240">Unleashing potential with Full-Stack Development
+                            solutions</div>
+                        <p class="paragraph text-center text-lg-start mt-4 innova-color-270">At Innova Soft Ltd., we believe in the power of
+                            Full-Stack Development to propel businesses forward. Our tailored solutions empower you to
+                            innovate, adapt, and thrive in a dynamic digital landscape. From concept to execution, trust us
+                            to transform your vision into reality.</p>
+                        <div class="text-center text-lg-start">
+                            <a href="<?php echo esc_url(home_url('index.php/contact-us')); ?>"
+                                class="btn card-btn mt-4 bg-innova-color-120 innova-color-10">Let's
+                                talk
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="3" stroke="currentColor" style="width: 18px; height: 16px;">
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+                                </svg>
+                            </a>
+                        </div>
                     </div>
+
                 </div>
             </div>
             <div class="col-12 col-lg-6 col-xl-6 d-none d-lg-block text-center">
                 <img class="img-fluid pb-3"
-                    src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/innovasoft-fullstack-dev-bnr.png"
+                    src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/fullstack-dev-banner.png"
                     alt="">
             </div>
         </div>
@@ -33,13 +45,13 @@
 <!-- header section ends  -->
 
 <!-- section 2 starts  -->
-<div style="background-color: #F7FBFB;" class="section-gap">
+<div class="section-gap bg-innova-color-10">
     <div class="container">
         <div class="d-flex justify-content-center align-items-center">
             <div class="py-1 text-center">
-                <div class="title">Explore Full-Stack Development: Unveil Our Process for Success</div>
+                <div class="title text-center innova-color-230">Explore Full-Stack Development: Unveil Our Process for Success</div>
                 <div class="d-flex justify-content-center align-items-center">
-                    <p class="paragraph mt-3 w-75">Discover the dynamic world of Full-Stack Development at Innova Soft
+                    <p class="paragraph mt-3 w-75 text-center innova-color-310">Discover the dynamic world of Full-Stack Development at Innova Soft
                         Ltd. Our process integrates advanced technologies and agile methodologies to deliver scalable
                         solutions that resonate with your audience.</p>
                 </div>
@@ -48,11 +60,11 @@
         <div class="row flex-lg-row flex-column p-3 mt-5">
             <div class="col-12 col-lg-5 d-flex justify-content-center align-items-center">
                 <img class="img-fluid"
-                    src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/innovasoft-fullstack-development-sub-section.png"
+                    src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/fullstack-development-sub-section.png"
                     alt="">
             </div>
             <div class="col-12 col-lg-7 mt-4">
-                <p class="paragraph mt-3 text-justify">
+                <p class="paragraph mt-3 text-justify innova-color-310">
                     Delve into the intricacies of Full-Stack Development, encompassing the entire software lifecycle
                     from requirement gathering to product deployment and maintenance. Our comprehensive approach ensures
                     seamless navigation through the following stages:
@@ -79,8 +91,8 @@
     <div class="container">
         <div class="d-flex justify-content-center align-items-center">
             <div class="py-3 text-center w-75">
-                <div class="title">Embark on your Full-Stack Development journey</div>
-                <p class="paragraph mt-3 text-center">Discover the streamlined process of onboarding for our Full-Stack
+                <div class="title text-center innova-color-230">Embark on your Full-Stack Development journey</div>
+                <p class="paragraph mt-3 text-center innova-color-310">Discover the streamlined process of onboarding for our Full-Stack
                     Development service.</p>
             </div>
         </div>
@@ -98,12 +110,12 @@
                             <div>
                                 <div class="circle1 position-relative" style="margin: 0 auto;">
                                     <span class="position-absolute translate-middle badge rounded-circle"
-                                        style="top: 20px; left: 15px; background-color: #8ECAA0;width: 22px; height:22px;">
+                                        style="top: 20px; left: 15px; background-color: #2a3ba0;width: 22px; height:22px;">
                                         1
                                     </span>
                                 </div>
-                                <h5 class="card-title card-margin-top ">Discovery Session</h5>
-                                <p class="card-text mb-2 ">Engage in an initial consultation where we understand your
+                                <h5 class="card-title card-margin-top innova-color-260">Discovery Session</h5>
+                                <p class="card-text mb-2 innova-color-270">Engage in an initial consultation where we understand your
                                     project requirements, objectives, and expectations. This session allows us to align
                                     our services with your vision and establish clear objectives for the development
                                     process.</p>
@@ -118,12 +130,12 @@
                             <div>
                                 <div class="circle2 position-relative" style="margin: 0 auto;">
                                     <span class="position-absolute translate-middle badge rounded-circle"
-                                        style="top: 20px; left: 15px; background-color: #8ECAA0;width: 22px; height:22px;">
+                                        style="top: 20px; left: 15px; background-color: #2a3ba0;width: 22px; height:22px;">
                                         2
                                     </span>
                                 </div>
-                                <h5 class="card-title card-margin-top ">Proposal Submission</h5>
-                                <p class="card-text mb-2">Following the initial consultation, we craft a tailored
+                                <h5 class="card-title card-margin-top innova-color-260">Proposal Submission</h5>
+                                <p class="card-text mb-2 innova-color-270">Following the initial consultation, we craft a tailored
                                     proposal outlining the scope of work, timelines, deliverables, and pricing
                                     structure. Our proposal is designed to address your specific needs and provide
                                     clarity on the development process and project milestones.</p>
@@ -137,12 +149,12 @@
                             <div>
                                 <div class="circle3 position-relative" style="margin: 0 auto;">
                                     <span class="position-absolute translate-middle badge rounded-circle"
-                                        style="top: 20px; left: 15px; background-color: #8ECAA0;width: 22px; height:22px;">
+                                        style="top: 20px; left: 15px; background-color: #2a3ba0;width: 22px; height:22px;">
                                         3
                                     </span>
                                 </div>
-                                <h5 class="card-title card-margin-top">Contract Drafting & Review</h5>
-                                <p class="card-text mb-2">Once the proposal is accepted, we proceed with drafting the
+                                <h5 class="card-title card-margin-top innova-color-260">Contract Drafting & Review</h5>
+                                <p class="card-text mb-2 innova-color-270">Once the proposal is accepted, we proceed with drafting the
                                     contract detailing the terms and conditions of the engagement. During this phase, we
                                     work closely with you to address any concerns, negotiate terms, and ensure mutual
                                     understanding and agreement before finalizing the contract.</p>
@@ -156,12 +168,12 @@
                             <div>
                                 <div class="circle4 position-relative" style="margin: 0 auto;">
                                     <span class="position-absolute translate-middle badge rounded-circle"
-                                        style="top: 20px; left: 15px; background-color: #8ECAA0;width: 22px; height:22px;">
+                                        style="top: 20px; left: 15px; background-color: #2a3ba0;width: 22px; height:22px;">
                                         4
                                     </span>
                                 </div>
-                                <h5 class="card-title card-margin-top">Sign-off and Onboarding</h5>
-                                <p class="card-text mb-2">Upon mutual agreement, the contract is signed by both parties,
+                                <h5 class="card-title card-margin-top innova-color-260">Sign-off and Onboarding</h5>
+                                <p class="card-text mb-2 innova-color-270">Upon mutual agreement, the contract is signed by both parties,
                                     signaling the official commencement of the Full-Stack Development project. Following
                                     contract execution, we initiate the onboarding process, where key stakeholders are
                                     introduced, project timelines are established, and initial project tasks are
@@ -182,15 +194,15 @@
     <div class="container">
         <div class="d-flex justify-content-center align-items-center">
             <div>
-                <div class="title text-center">FAQ (Frequently Asked Question)</div>
-                <p class="paragraph mt-3 text-center">Explore commonly asked questions about our Full-Stack Development
+                <div class="title text-center innova-color-230">FAQ (Frequently Asked Question)</div>
+                <p class="paragraph mt-3 text-center innova-color-310">Explore commonly asked questions about our Full-Stack Development
                     service.</p>
             </div>
         </div>
         <div class="d-flex justify-content-center align-items-center">
             <div class="accordion w-75" id="accordionExample">
                 <div class="accordion-item my-3">
-                    <h2 class="accordion-header faq-title" id="headingOne">
+                    <h2 class="accordion-header faq-title innova-color-320" id="headingOne">
                         <button class="accordion-button" type="button" data-bs-toggle="collapse"
                             data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                             1. What technologies do you specialize in?
@@ -198,14 +210,14 @@
                     </h2>
                     <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne"
                         data-bs-parent="#accordionExample">
-                        <div class="accordion-body faq-details">
+                        <div class="accordion-body faq-details innova-color-270">
                             Our expertise spans across PHP, .NET, Java, Python, JavaScript frameworks e.g. Node.js,
                             Python, React, Angular, and more.
                         </div>
                     </div>
                 </div>
                 <div class="accordion-item my-3">
-                    <h2 class="accordion-header faq-title" id="headingTwo">
+                    <h2 class="accordion-header faq-title innova-color-320" id="headingTwo">
                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                             data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                             2. Can you handle both web and mobile development?
@@ -213,13 +225,13 @@
                     </h2>
                     <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
                         data-bs-parent="#accordionExample">
-                        <div class="accordion-body faq-details">
+                        <div class="accordion-body faq-details innova-color-270">
                             Absolutely, we offer comprehensive solutions for both web and mobile platforms.
                         </div>
                     </div>
                 </div>
                 <div class="accordion-item my-3">
-                    <h2 class="accordion-header faq-title" id="headingThree">
+                    <h2 class="accordion-header faq-title innova-color-320" id="headingThree">
                         <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                             data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                             3. How do you ensure scalability and performance?
@@ -227,7 +239,7 @@
                     </h2>
                     <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
                         data-bs-parent="#accordionExample">
-                        <div class="accordion-body faq-details">
+                        <div class="accordion-body faq-details innova-color-270">
                             We employ industry best practices and conduct rigorous testing to ensure scalability and
                             performance optimization.
                         </div>
@@ -240,18 +252,18 @@
 <!-- FAQ section ends  -->
 
 <!-- conatct us sectoin starts  -->
-<div class="my-5" style="background-color: #EAFFE4;">
+<div class="connect-section bg-innova-color-260">
     <div class="container py-4">
         <div class="dots-block-left">
             <div class="dots-block-right">
                 <div class="d-flex justify-content-center align-items-center p-5">
                     <div>
-                        <div class="title text-center" style="color: #43A661;">
+                        <div class="title text-center innova-color-10">
                             Elevate Your Project Today
                         </div>
-                        <p class="paragraph mt-3 text-center">Connect with us to transform your vision into reality.</p>
+                        <p class="paragraph mt-3 text-center innova-color-10">Connect with us to transform your vision into reality.</p>
                         <div class="text-center">
-                            <a href="<?php echo esc_url(home_url('index.php/contact-us')); ?>" class="btn btn-success card-btn"
+                            <a href="<?php echo esc_url(home_url('index.php/contact-us')); ?>" class="btn connect-btn bg-innova-color-140 innova-color-10"
                                 style="margin-top: 10px;">Let's
                                 talk <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                     stroke-width="3" stroke="currentColor" style="width: 18px; height: 16px;">
